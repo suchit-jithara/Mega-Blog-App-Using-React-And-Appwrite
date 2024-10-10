54d46a948ca25d48fcc250be0fe67929691bec64
Suchit Jithara <suchitatul312@gmail.com>
2024-10-10 16:13:04 +0530

    fourth commit

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Editor } from '@tinymce/tinymce-react';  
 import {Controller } from 'react-hook-form';
-
+import conf from "../conf/conf";
 
 export default function RTE({name, control, label, defaultValue =""}) {
   return (
@@ -13,7 +13,7 @@ export default function RTE({name, control, label, defaultValue =""}) {
     control={control}
     render={({field: {onChange}}) => (
         <Editor
-        apiKey='gsccdmk1hk1yrzn84mbqzx3rztmllke50sl1i9mht5dh9lko'
+        apiKey={conf.tinyMCEApiKey}
         initialValue={defaultValue}
         init={{
             initialValue: defaultValue,
